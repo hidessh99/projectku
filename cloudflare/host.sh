68c475912ecbc115f14a039be3773d8ed9ec7f0d
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2022-03-26 22:31:13 +0700

    Create host.sh

---
+++ cloudflare/host.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- icanhazip.com);
+apt install jq curl -y
+
+rm -f /root/domain
+rm -f /etc/v2ray/domain
+rm -f /etc/xray/domain
+rm -rf /etc/xray/domain
+mkdir -p /usr/bin/xray
+mkdir -p /etc/xray
+cp /root/domain /etc/xray/
+
+DOMAIN=hideserver.xyz
+read -rp "Masukkan Subdomain: " -e sub
+SUB_DOMAIN=${sub}.${DOMAIN}
+CF_ID=dedi4susanto@gmail.com
+CF_KEY=78b3613d3d54457a4fb5ef28b066b3fe66abe
+
+echo "IP=""$SUB_DOMAIN" >> /var/lib/crot/ipvps.conf
+
+set -euo pipefail
+IP=$(wget -qO- icanhazip.com);
+echo "Updating DNS for ${SUB_DOMAIN}..."
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+if [[ "${#RECORD}" -le 10 ]]; then
+     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
+fi
+
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
+echo "Host : $SUB_DOMAIN"
+echo $SUB_DOMAIN > /root/domain
+cp /root/domain /etc/xray/
+echo $SUB_DOMAIN > /etc/v2ray/domain
+echo $SUB_DOMAIN > /etc/xray/domain
+echo $SUB_DOMAIN > /root/domain
+echo $SUB_DOMAIN > /etc/xray/domain
