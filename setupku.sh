aaa160ac1c4f7abdfab3b1e01e724f8f197a4d4d
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2022-03-26 22:22:29 +0700

    Update setupku.sh

--- setupku.sh
+++ setupku.sh
@@ -53,8 +53,11 @@ if [ -f "/etc/xray/domain" ]; then
 echo "Script Already Installed"
 exit 0
 fi
+
 mkdir /var/lib/crot;
 echo "IP=" >> /var/lib/crot/ipvps.conf
+wget https://${akbarvpn}/slhost.sh && chmod +x slhost.sh && ./slhost.sh
+
 #install xray
 wget https://${akbarvpnnnnnn}/ins-xray.sh && chmod +x ins-xray.sh && screen -S xray ./ins-xray.sh
 
