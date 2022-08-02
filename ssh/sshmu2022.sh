8d48e0d0758d383740bc488b76581c8d822fe943
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2022-08-02 23:53:14 +0700

    Update sshmu2022.sh

--- ssh/sshmu2022.sh
+++ ssh/sshmu2022.sh
@@ -185,7 +185,7 @@ echo "<?php phpinfo() ?>" > /home/vps/public_html/info.php
 chown -R www-data:www-data /home/vps/public_html
 chmod -R g+rw /home/vps/public_html
 cd /home/vps/public_html
-wget -O /home/vps/public_html/index.html "https://${akbarvpn}/index.html1"
+wget -O /home/vps/public_html/index.html "https://raw.githubusercontent.com/hidessh99/projectku/main/hide-index.html"
 /etc/init.d/nginx restart
 cd
 
@@ -434,7 +434,6 @@ wget -O port-sstp "https://gitlab.com/hidessh/baru/-/raw/main/port-sstp.sh"
 wget -O port-squid "https://gitlab.com/hidessh/baru/-/raw/main/port-squid.sh"
 wget -O port-ws "https://gitlab.com/hidessh/baru/-/raw/main/port-ws.sh"
 wget -O port-vless "https://gitlab.com/hidessh/baru/-/raw/main/port-vless.sh"
-wget -O wbmn "https://gitlab.com/hidessh/baru/-/raw/main/webmin.sh"
 wget -O xp "https://gitlab.com/hidessh/baru/-/raw/main/xp.sh"
 wget -O update "https://gitlab.com/hidessh/baru/-/raw/main/update.sh"
 wget -O user-limit "https://gitlab.com/hidessh/baru/-/raw/main/user-limit.sh"
