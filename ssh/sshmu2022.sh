d0d164d62d39a7facd9f96c01b9498f2d9772e77
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2022-08-02 23:02:48 +0700

    Update sshmu2022.sh

--- ssh/sshmu2022.sh
+++ ssh/sshmu2022.sh
@@ -371,6 +371,21 @@ echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
 
 # blockir torrent
 apt install iptables-persistent -y
+
+iptables -A FORWARD -m string --string "get_peers" --algo bm -j DROP
+iptables -A FORWARD -m string --string "announce_peer" --algo bm -j DROP
+iptables -A FORWARD -m string --string "find_node" --algo bm -j DROP
+iptables -A FORWARD -m string --algo bm --string "BitTorrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "BitTorrent protocol" -j DROP
+iptables -A FORWARD -m string --algo bm --string "peer_id=" -j DROP
+iptables -A FORWARD -m string --algo bm --string ".torrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "announce.php?passkey=" -j DROP
+iptables -A FORWARD -m string --algo bm --string "torrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "announce" -j DROP
+iptables -A FORWARD -m string --algo bm --string "info_hash" -j DROP
+iptables-save > /etc/iptables.up.rules
+iptables-restore -t < /etc/iptables.up.rules
+
 #wget https://raw.githubusercontent.com/4hidessh/hidessh/main/security/torrent && chmod +x torrent && ./torrent
 #iptables-save > /etc/iptables.up.rules
 #iptables-restore -t < /etc/iptables.up.rules
