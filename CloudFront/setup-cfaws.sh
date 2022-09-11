9fac43cb321ce86fa30b62a4644974df78787f16
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2022-09-11 11:08:30 +0700

    Update setup-cfaws.sh

--- CloudFront/setup-cfaws.sh
+++ CloudFront/setup-cfaws.sh
@@ -10,7 +10,7 @@ mkdir mrbogel
 cd
 # Cloudfront
 #port 5000
-wget -O /etc/mrbogel/proxy3.js https://github.com/hidessh99/projectku/blob/main/CloudFront/proxy3.js && chmod +x /etc/mrbogel/proxy3.js
+wget -O /etc/mrbogel/proxy3.js https://raw.githubusercontent.com/hidessh99/projectku/main/CloudFront/proxy3.js && chmod +x /etc/mrbogel/proxy3.js
 wget -O /etc/systemd/system/nodews1.service https://raw.githubusercontent.com/hidessh99/projectku/main/CloudFront/nodews1.service && chmod +x /etc/systemd/system/nodews1.service
 #reboot service
 systemctl daemon-reload
