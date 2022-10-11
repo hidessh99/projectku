36017560977558034abb9694f06bc1900318bef6
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2022-10-11 22:16:51 +0700

    Create slodns-eror.sh

---
+++ Slowdns/slodns-eror.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Created by HideSSH
+#auto script SlowDNS Eror
+
+
+pkill sldns-server
+pkill sldns-client
+
+systemctl daemon-reload
+systemctl stop client-sldns
+systemctl stop server-sldns
+
+systemctl enable client-sldns
+systemctl enable server-sldns
+
+systemctl start client-sldns
+systemctl start server-sldns
+
+systemctl restart client-sldns
+systemctl restart server-sldns
+
+sleep 5
